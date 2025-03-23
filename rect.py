9dc12e6ad7f98e8b8462ac84f5b4a197dac74c1f
kkunishy <128043057+kkunishy@users.noreply.github.com>
2025-03-23 11:43:24 +0900

    asdf

--- rect.py
+++ rect.py
@@ -2,7 +2,11 @@ import random as rd
 import turtle as t
 a=0
 
+def aRand():
+    a = rd.randrange(1, 1000)
+
 while True:
-    t.forward(30)
-    t.tiltangle(10+10*a)
-    a=a+1
+    aRand()
+    t.forward(5+a)
+    aRand()
+    t.left(a)
